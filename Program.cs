53390b900dd0eb25259a38e21390c85dd5203b73
Dem4e <Dem4e@LAPTOP-3L3S3FH2>
2023-02-12 21:28:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace задание_2._30
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int g = 465;
+            var h = g.ToString().ToList();
+            char ch2 = h[1];
+            h.Remove(ch2);
+            h.Add(ch2);
+            string resultStr = String.Join("", h);
+            int resultInt = int.Parse(resultStr);
+            Console.WriteLine(resultInt);
+
+        }
+    }
+}
